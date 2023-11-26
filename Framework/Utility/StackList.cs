c197a81d627eb900a1cb43fd032c00f6d1004641
Noel Berry <thenberry@gmail.com>
2023-11-25 17:23:58 -0800

    using new C#12 InlineArray for StackList

--- Framework/Utility/StackList.cs
+++ Framework/Utility/StackList.cs
@@ -1,28 +1,33 @@
 using System.Collections;
+using System.Runtime.CompilerServices;
 
 namespace Foster.Framework;
 
+/// <summary>
+/// StackList is a simple stack-allocated list, useful for holding small arrays
+/// of data on the stack without allocating heap memory. They do not expand 
+/// past their specified capacity.
+/// TODO: each capacity could be code-generated instead of copy+pasted
+/// </summary>
 public struct StackList8<T> : IEnumerable<T>, IList<T>
 {
 	public const int TypeCapacity = 8;
 
-	T v0; T v1; T v2; T v3; T v4; T v5; T v6; T v7;
-	
+	[InlineArray(TypeCapacity)]
+	private struct Elements { private T _element0; }
+
+	private Elements elements;
 	private int count;
 
-	public int Count => count;
-	public int Capacity => TypeCapacity;
-	public bool IsReadOnly => false;
+	public readonly int Count => count;
+	public readonly int Capacity => TypeCapacity;
+	public readonly bool IsReadOnly => false;
 
 	public void Add(T value)
 	{
 		if (count >= Capacity)
 			throw new OutOfMemoryException();
-		count++;
-		this[count - 1] = value;
-
-		var test = new List<int>();
-		test.IndexOf(10);
+		elements[count++] = value;
 	}
 
 	public void Resize(int count)
@@ -37,120 +42,114 @@ public struct StackList8<T> : IEnumerable<T>, IList<T>
 		if (index < 0 || index >= count)
 			throw new IndexOutOfRangeException();
 		for (int i = index; i < count - 1; i ++)
-			this[i] = this[i + 1];
+			elements[i] = elements[i + 1];
 		count--;
 	}
 
 	public void Clear()
 	{
 		for (int i = 0; i < count; i++)
-			this[i] = default(T)!;
+			elements[i] = default!;
 		count = 0;
 	}
 
-	public int IndexOf(T item)
+	public readonly int IndexOf(T item)
 	{
-		throw new NotImplementedException();
+		for (int i = 0; i < count; i ++)
+			if (EqualityComparer<T>.Default.Equals(elements[i], item))
+				return i;
+		return -1;
 	}
 
 	public void Insert(int index, T item)
 	{
-		throw new NotImplementedException();
+		if (count >= Capacity)
+			throw new OutOfMemoryException();
+		for (int i = count - 1; i > index; i --)
+			elements[i] = elements[i - 1];
+		elements[index] = item;
 	}
 
-	public bool Contains(T item)
-	{
-		throw new NotImplementedException();
-	}
+	public readonly bool Contains(T item)
+		=> IndexOf(item) >= 0;
 
-	public void CopyTo(T[] array, int arrayIndex)
+	public readonly void CopyTo(T[] array, int arrayIndex)
 	{
-		throw new NotImplementedException();
+		for (int i = 0; i < count; i ++)
+			array[arrayIndex + i] = elements[i];
 	}
 
 	public bool Remove(T item)
 	{
-		throw new NotImplementedException();
+		var index = IndexOf(item);
+		if (index >= 0)
+		{
+			for (int i = index; i < count - 1; i ++)
+				elements[i] = elements[i + 1];
+			count--;
+			elements[count] = default!;
+			return true;
+		}
+		return false;
 	}
 
 	public T this[int index]
 	{
-		get
+		readonly get
 		{
-			if (index < count)
-			{
-				switch (index)
-				{
-					case 0: return v0; case 1: return v1; case 2: return v2; case 3: return v3; 
-					case 4: return v4; case 5: return v5; case 6: return v6; case 7: return v7;
-				}
-			}
-			throw new IndexOutOfRangeException();
+			if (index >= count)
+				throw new IndexOutOfRangeException();
+			return elements[index];
 		}
 		set
 		{
-			if (index < count)
-			{
-				switch (index)
-				{
-					case 0: v0 = value; return; case 1: v1 = value; return; case 2: v2 = value; return; case 3: v3 = value; return;
-					case 4: v4 = value; return; case 5: v5 = value; return; case 6: v6 = value; return; case 7: v7 = value; return;
-				}
-			}
-			throw new IndexOutOfRangeException();
+			if (index >= count)
+				throw new IndexOutOfRangeException();
+			elements[index] = value;
 		}
 	}
 
-	public Enumerator GetEnumerator() => new(this);
+	public readonly Enumerator GetEnumerator() => new(this);
 
-	IEnumerator<T> IEnumerable<T>.GetEnumerator() => throw new NotImplementedException("Boxing Value");
-	IEnumerator IEnumerable.GetEnumerator() => throw new NotImplementedException("Boxing Value");
+	readonly IEnumerator<T> IEnumerable<T>.GetEnumerator()
+		=> new Enumerator(this);
 
-	public struct Enumerator : IEnumerator<T>
+	readonly IEnumerator IEnumerable.GetEnumerator()
+		=> new Enumerator(this);
+
+	public struct Enumerator(in StackList8<T> list) : IEnumerator<T>
 	{
-		StackList8<T> list;
-		int index;
+		private StackList8<T> list = list;
+		private int index = -1;
 
-		public Enumerator(in StackList8<T> list)
-		{
-			this.list = list;
-			this.index = -1;
-		}
+		public readonly T Current => list[index];
+        readonly object IEnumerator.Current => Current!;
 
-		public T Current => list[index];
-		object IEnumerator.Current => throw new NotImplementedException("Boxing Value");
-
-		public void Dispose() { }
-
-		public bool MoveNext()
-		{
-			index++;
-			return index < list.Count;
-		}
-
-		public void Reset()
-		{
-			index = -1;
-		}
-	}
+		public readonly void Dispose() { }
+        public bool MoveNext() => (++index) < list.Count;
+        public void Reset() => index = -1;
+    }
 }
 
-public struct StackList16<T>
+public struct StackList16<T> : IEnumerable<T>, IList<T>
 {
-	public const int Capacity = 16;
+	public const int TypeCapacity = 16;
 
-	T v0; T v1; T v2; T v3; T v4; T v5; T v6; T v7;
-	T v8; T v9; T v10; T v11; T v12; T v13; T v14; T v15;
-	
+	[InlineArray(TypeCapacity)]
+	private struct Elements { private T _element0; }
+
+	private Elements elements;
 	private int count;
-	public int Count => count;
+
+	public readonly int Count => count;
+	public readonly int Capacity => TypeCapacity;
+	public readonly bool IsReadOnly => false;
 
 	public void Add(T value)
 	{
 		if (count >= Capacity)
 			throw new OutOfMemoryException();
-		count++;
-		this[count - 1] = value;
+		elements[count++] = value;
 	}
 
 	public void Resize(int count)
@@ -164,40 +163,336 @@ public struct StackList16<T>
 	{
 		if (index < 0 || index >= count)
 			throw new IndexOutOfRangeException();
-		for (int i = index; i < count; i ++)
-			this[i] = this[i + 1];
+		for (int i = index; i < count - 1; i ++)
+			elements[i] = elements[i + 1];
 		count--;
+	}
+
+	public void Clear()
+	{
+		for (int i = 0; i < count; i++)
+			elements[i] = default!;
+		count = 0;
+	}
+
+	public readonly int IndexOf(T item)
+	{
+		for (int i = 0; i < count; i ++)
+			if (EqualityComparer<T>.Default.Equals(elements[i], item))
+				return i;
+		return -1;
+	}
+
+	public void Insert(int index, T item)
+	{
+		if (count >= Capacity)
+			throw new OutOfMemoryException();
+		for (int i = count - 1; i > index; i --)
+			elements[i] = elements[i - 1];
+		elements[index] = item;
+	}
+
+	public readonly bool Contains(T item)
+		=> IndexOf(item) >= 0;
+
+	public readonly void CopyTo(T[] array, int arrayIndex)
+	{
+		for (int i = 0; i < count; i ++)
+			array[arrayIndex + i] = elements[i];
+	}
+
+	public bool Remove(T item)
+	{
+		var index = IndexOf(item);
+		if (index >= 0)
+		{
+			for (int i = index; i < count - 1; i ++)
+				elements[i] = elements[i + 1];
+			count--;
+			elements[count] = default!;
+			return true;
+		}
+		return false;
 	}
 
 	public T this[int index]
 	{
-		get
+		readonly get
 		{
-			if (index < count)
-			{
-				switch (index)
-				{
-					case 0: return v0; case 1: return v1; case 2: return v2; case 3: return v3; 
-					case 4: return v4; case 5: return v5; case 6: return v6; case 7: return v7;
-					case 8: return v8; case 9: return v9; case 10: return v10; case 11: return v11;
-					case 12: return v12; case 13: return v13; case 14: return v14; case 15: return v15;
-				}
-			}
-			throw new IndexOutOfRangeException();
+			if (index >= count)
+				throw new IndexOutOfRangeException();
+			return elements[index];
 		}
 		set
 		{
-			if (index < count)
-			{
-				switch (index)
-				{
-					case 0: v0 = value; return; case 1: v1 = value; return; case 2: v2 = value; return; case 3: v3 = value; return; 
-					case 4: v4 = value; return; case 5: v5 = value; return; case 6: v6 = value; return; case 7: v7 = value; return;
-					case 8: v8 = value; return; case 9: v9 = value; return; case 10: v10 = value; return; case 11: v11 = value; return;
-					case 12: v12 = value; return; case 13: v13 = value; return; case 14: v14 = value; return; case 15: v15 = value; return;
-				}
-			}
-			throw new IndexOutOfRangeException();
+			if (index >= count)
+				throw new IndexOutOfRangeException();
+			elements[index] = value;
 		}
 	}
+
+	public readonly Enumerator GetEnumerator() => new(this);
+
+	readonly IEnumerator<T> IEnumerable<T>.GetEnumerator()
+		=> new Enumerator(this);
+
+	readonly IEnumerator IEnumerable.GetEnumerator()
+		=> new Enumerator(this);
+
+	public struct Enumerator(in StackList16<T> list) : IEnumerator<T>
+	{
+		private StackList16<T> list = list;
+		private int index = -1;
+
+		public readonly T Current => list[index];
+        readonly object IEnumerator.Current => Current!;
+
+		public readonly void Dispose() { }
+        public bool MoveNext() => (++index) < list.Count;
+        public void Reset() => index = -1;
+    }
+}
+
+public struct StackList32<T> : IEnumerable<T>, IList<T>
+{
+	public const int TypeCapacity = 32;
+
+	[InlineArray(TypeCapacity)]
+	private struct Elements { private T _element0; }
+
+	private Elements elements;
+	private int count;
+
+	public readonly int Count => count;
+	public readonly int Capacity => TypeCapacity;
+	public readonly bool IsReadOnly => false;
+
+	public void Add(T value)
+	{
+		if (count >= Capacity)
+			throw new OutOfMemoryException();
+		elements[count++] = value;
+	}
+
+	public void Resize(int count)
+	{
+		if (count >= Capacity)
+			throw new OutOfMemoryException();
+		this.count = count;
+	}
+
+	public void RemoveAt(int index)
+	{
+		if (index < 0 || index >= count)
+			throw new IndexOutOfRangeException();
+		for (int i = index; i < count - 1; i ++)
+			elements[i] = elements[i + 1];
+		count--;
+	}
+
+	public void Clear()
+	{
+		for (int i = 0; i < count; i++)
+			elements[i] = default!;
+		count = 0;
+	}
+
+	public readonly int IndexOf(T item)
+	{
+		for (int i = 0; i < count; i ++)
+			if (EqualityComparer<T>.Default.Equals(elements[i], item))
+				return i;
+		return -1;
+	}
+
+	public void Insert(int index, T item)
+	{
+		if (count >= Capacity)
+			throw new OutOfMemoryException();
+		for (int i = count - 1; i > index; i --)
+			elements[i] = elements[i - 1];
+		elements[index] = item;
+	}
+
+	public readonly bool Contains(T item)
+		=> IndexOf(item) >= 0;
+
+	public readonly void CopyTo(T[] array, int arrayIndex)
+	{
+		for (int i = 0; i < count; i ++)
+			array[arrayIndex + i] = elements[i];
+	}
+
+	public bool Remove(T item)
+	{
+		var index = IndexOf(item);
+		if (index >= 0)
+		{
+			for (int i = index; i < count - 1; i ++)
+				elements[i] = elements[i + 1];
+			count--;
+			elements[count] = default!;
+			return true;
+		}
+		return false;
+	}
+
+	public T this[int index]
+	{
+		readonly get
+		{
+			if (index >= count)
+				throw new IndexOutOfRangeException();
+			return elements[index];
+		}
+		set
+		{
+			if (index >= count)
+				throw new IndexOutOfRangeException();
+			elements[index] = value;
+		}
+	}
+
+	public readonly Enumerator GetEnumerator() => new(this);
+
+	readonly IEnumerator<T> IEnumerable<T>.GetEnumerator()
+		=> new Enumerator(this);
+
+	readonly IEnumerator IEnumerable.GetEnumerator()
+		=> new Enumerator(this);
+
+	public struct Enumerator(in StackList32<T> list) : IEnumerator<T>
+	{
+		private StackList32<T> list = list;
+		private int index = -1;
+
+		public readonly T Current => list[index];
+        readonly object IEnumerator.Current => Current!;
+
+		public readonly void Dispose() { }
+        public bool MoveNext() => (++index) < list.Count;
+        public void Reset() => index = -1;
+    }
+}
+
+public struct StackList64<T> : IEnumerable<T>, IList<T>
+{
+	public const int TypeCapacity = 64;
+
+	[InlineArray(TypeCapacity)]
+	private struct Elements { private T _element0; }
+
+	private Elements elements;
+	private int count;
+
+	public readonly int Count => count;
+	public readonly int Capacity => TypeCapacity;
+	public readonly bool IsReadOnly => false;
+
+	public void Add(T value)
+	{
+		if (count >= Capacity)
+			throw new OutOfMemoryException();
+		elements[count++] = value;
+	}
+
+	public void Resize(int count)
+	{
+		if (count >= Capacity)
+			throw new OutOfMemoryException();
+		this.count = count;
+	}
+
+	public void RemoveAt(int index)
+	{
+		if (index < 0 || index >= count)
+			throw new IndexOutOfRangeException();
+		for (int i = index; i < count - 1; i ++)
+			elements[i] = elements[i + 1];
+		count--;
+	}
+
+	public void Clear()
+	{
+		for (int i = 0; i < count; i++)
+			elements[i] = default!;
+		count = 0;
+	}
+
+	public readonly int IndexOf(T item)
+	{
+		for (int i = 0; i < count; i ++)
+			if (EqualityComparer<T>.Default.Equals(elements[i], item))
+				return i;
+		return -1;
+	}
+
+	public void Insert(int index, T item)
+	{
+		if (count >= Capacity)
+			throw new OutOfMemoryException();
+		for (int i = count - 1; i > index; i --)
+			elements[i] = elements[i - 1];
+		elements[index] = item;
+	}
+
+	public readonly bool Contains(T item)
+		=> IndexOf(item) >= 0;
+
+	public readonly void CopyTo(T[] array, int arrayIndex)
+	{
+		for (int i = 0; i < count; i ++)
+			array[arrayIndex + i] = elements[i];
+	}
+
+	public bool Remove(T item)
+	{
+		var index = IndexOf(item);
+		if (index >= 0)
+		{
+			for (int i = index; i < count - 1; i ++)
+				elements[i] = elements[i + 1];
+			count--;
+			elements[count] = default!;
+			return true;
+		}
+		return false;
+	}
+
+	public T this[int index]
+	{
+		readonly get
+		{
+			if (index >= count)
+				throw new IndexOutOfRangeException();
+			return elements[index];
+		}
+		set
+		{
+			if (index >= count)
+				throw new IndexOutOfRangeException();
+			elements[index] = value;
+		}
+	}
+
+	public readonly Enumerator GetEnumerator() => new(this);
+
+	readonly IEnumerator<T> IEnumerable<T>.GetEnumerator()
+		=> new Enumerator(this);
+
+	readonly IEnumerator IEnumerable.GetEnumerator()
+		=> new Enumerator(this);
+
+	public struct Enumerator(in StackList64<T> list) : IEnumerator<T>
+	{
+		private StackList64<T> list = list;
+		private int index = -1;
+
+		public readonly T Current => list[index];
+        readonly object IEnumerator.Current => Current!;
+
+		public readonly void Dispose() { }
+        public bool MoveNext() => (++index) < list.Count;
+        public void Reset() => index = -1;
+    }
 }
