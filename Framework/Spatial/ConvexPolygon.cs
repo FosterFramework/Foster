003f0da87b170eef4a5b24465a974cd8a3230870
Noel Berry <thenberry@gmail.com>
2023-11-25 17:48:03 -0800

    Refactoring ConvexPolygon to use new C#12 InlineArray

--- Framework/Spatial/ConvexPolygon.cs
+++ Framework/Spatial/ConvexPolygon.cs
@@ -1,109 +1,84 @@
-﻿using System.Diagnostics;
+﻿using System.Collections;
+using System.Diagnostics;
 using System.Numerics;
 using System.Runtime.InteropServices;
 
 namespace Foster.Framework;
 
 [StructLayout(LayoutKind.Sequential)]
-public unsafe struct ConvexPolygon : IConvexShape
+public unsafe struct ConvexPolygon : IConvexShape, IEnumerable<Vector2>
 {
-	public const int MaxPoints = 32;
+	public const int MaxPoints = StackList32<Vector2>.TypeCapacity;
+	public StackList32<Vector2> Vertices;
 
-	private fixed float points[MaxPoints * 2];
-	private int pointCount;
-
-	public int Points
-	{
-		readonly get => pointCount;
-		set
-		{
-			Debug.Assert(value >= 0 && value < MaxPoints);
-			pointCount = value;
-		}
-	}
-
-	public int Axes
-	{
-		readonly get => pointCount;
-		set
-		{
-			Debug.Assert(value >= 0 && value < MaxPoints);
-			pointCount = value;
-		}
-	}
+	public readonly int Points => Vertices.Count;
+	public readonly int Axes => Vertices.Count;
 
 	public readonly Rect Bounds
 	{
 		get
 		{
-			Rect bounds = new(this[0].X, this[0].Y, 0, 0);
+			Rect bounds = new(Vertices[0].X, Vertices[0].Y, 0, 0);
 
 			for (int i = 1; i < Points; i++)
 			{
-				if (this[i].X < bounds.X)
+				if (Vertices[i].X < bounds.X)
 				{
-					bounds.Width += bounds.X - this[i].X;
-					bounds.X = this[i].X;
+					bounds.Width += bounds.X - Vertices[i].X;
+					bounds.X = Vertices[i].X;
 				}
 
-				if (this[i].X > bounds.Right)
-					bounds.Width = this[i].X - bounds.X;
+				if (Vertices[i].X > bounds.Right)
+					bounds.Width = Vertices[i].X - bounds.X;
 
-				if (this[i].Y < bounds.Y)
+				if (Vertices[i].Y < bounds.Y)
 				{
-					bounds.Height += bounds.Y - this[i].Y;
-					bounds.Y = this[i].Y;
+					bounds.Height += bounds.Y - Vertices[i].Y;
+					bounds.Y = Vertices[i].Y;
 				}
 
-				if (this[i].Y > bounds.Bottom)
-					bounds.Height = this[i].Y - bounds.Y;
+				if (Vertices[i].Y > bounds.Bottom)
+					bounds.Height = Vertices[i].Y - bounds.Y;
 			}	
 
 			return bounds;
 		}
 	}
+
+	public void Add(in Vector2 value)
+		=> Vertices.Add(value);
+
+	public void RemoveAt(int index)
+		=> Vertices.RemoveAt(index);
 	
+	[Obsolete("Use ConvexPolygon.Add")]
 	public void AddPoint(in Vector2 value)
-	{
-		Debug.Assert(pointCount < MaxPoints);
-		SetPoint(pointCount, value);
-		pointCount++;
-	}
+		=> Vertices.Add(value);
 	
+	[Obsolete("Use ConvexPolygon.RemoveAt")]
 	public void RemovePointAt(int index)
-	{
-		Debug.Assert(index >= 0 && index < pointCount);
-		for (int i = index; i < pointCount - 1; i ++)
-			SetPoint(i, GetPoint(i + 1));
-		pointCount--;
-	}
+		=> Vertices.RemoveAt(index);
 
-	public void SetPoint(int index, Vector2 position)
-	{
-		Debug.Assert(index >= 0 && index < MaxPoints);
+	[Obsolete("Use ConvexPolygon.this[int index]")]
+	public void SetPoint(int index, Vector2 value)
+		=> Vertices[index] = value;
 
-		points[index * 2 + 0] = position.X;
-		points[index * 2 + 1] = position.Y;
-	}
-
+	[Obsolete("Use ConvexPolygon.this[int index]")]
 	public readonly Vector2 GetPoint(int index)
-	{
-		Debug.Assert(index >= 0 && index < MaxPoints);
-		return new(points[index * 2 + 0], points[index * 2 + 1]);
-	}
+		=> Vertices[index];
 
 	public Vector2 this[int index]
 	{
-		readonly get => GetPoint(index);
-		set => SetPoint(index, value);
+		readonly get => Vertices[index];
+		set => Vertices[index] = value;
 	}
 
 	public readonly Vector2 GetAxis(int index)
 	{
 		Debug.Assert(index >= 0 && index < MaxPoints);
 
-		var a = GetPoint(index);
-		var b = GetPoint(index >= pointCount - 1 ? 0 : index + 1);
+		var a = Vertices[index];
+		var b = Vertices[index >= Vertices.Count - 1 ? 0 : index + 1];
 		var normal = (b - a).Normalized();
 
 		return new(-normal.Y, normal.X);
@@ -113,24 +88,22 @@ public unsafe struct ConvexPolygon : IConvexShape
 	{
 		int total = 0;
 		for (int i = 0; i < Points; i++)
-			total += Calc.Orient(GetPoint(i), GetPoint((i + 1) % Points), vec2);
+			total += Calc.Orient(Vertices[i], Vertices[(i + 1) % Vertices.Count], vec2);
 		return Math.Abs(total) == Points;
 	}
 
 	public readonly void Project(in Vector2 axis, out float min, out float max)
 	{
-		if (pointCount <= 0)
+		if (Vertices.Count <= 0)
 		{
 			min = max = 0;
 		}
 		else
 		{
-			min = float.MaxValue;
-			max = float.MinValue;
-
-			for (int i = 0; i < pointCount; i++)
+			min = max = Vector2.Dot(Vertices[0], axis);
+			for (int i = 1; i < Vertices.Count; i++)
 			{
-				var dot = Vector2.Dot(new Vector2(points[i * 2 + 0], points[i * 2 + 1]), axis);
+				var dot = Vector2.Dot(Vertices[i], axis);
 				min = Math.Min(dot, min);
 				max = Math.Max(dot, max);
 			}
@@ -148,12 +121,12 @@ public unsafe struct ConvexPolygon : IConvexShape
 		if (reverse)
 		{
 			for (int i = 0; i < polygon.Points; i ++)
-				result.AddPoint(Vector2.Transform(polygon.GetPoint(polygon.Points - i - 1), matrix));
+				result.Vertices.Add(Vector2.Transform(polygon.Vertices[polygon.Points - i - 1], matrix));
 		}
 		else
 		{
 			for (int i = 0; i < polygon.Points; i ++)
-				result.AddPoint(Vector2.Transform(polygon.GetPoint(i), matrix));
+				result.Vertices.Add(Vector2.Transform(polygon.Vertices[i], matrix));
 		}
 
 		return result;
@@ -161,11 +134,11 @@ public unsafe struct ConvexPolygon : IConvexShape
 
 	public static bool operator ==(in ConvexPolygon a, in ConvexPolygon b)
 	{
-		if (a.pointCount != b.pointCount)
+		if (a.Vertices.Count != b.Vertices.Count)
 			return false;
 
-		for (int i = 0, n = a.pointCount * 2; i < n; i ++)
-			if (a.points[i] != b.points[i])
+		for (int i = 0; i < a.Vertices.Count; i ++)
+			if (a.Vertices[i] != b.Vertices[i])
 				return false;
 
 		return true;
@@ -176,31 +149,36 @@ public unsafe struct ConvexPolygon : IConvexShape
 	public static ConvexPolygon operator +(in ConvexPolygon a, in Vector2 b)
 	{
 		ConvexPolygon result = a;
-		for (int i = 0; i < result.pointCount * 2; i += 2)
-		{
-			result.points[i] += b.X;
-			result.points[i + 1] += b.Y;
-		}
+		for (int i = 0; i < result.Vertices.Count; i ++)
+			result.Vertices[i] += b;
 		return result;
 	}
 
 	public static ConvexPolygon operator -(in ConvexPolygon a, in Vector2 b)
 	{
 		ConvexPolygon result = a;
-		for (int i = 0; i < result.pointCount * 2; i += 2)
-		{
-			result.points[i] -= b.X;
-			result.points[i + 1] -= b.Y;
-		}
+		for (int i = 0; i < result.Vertices.Count; i ++)
+			result.Vertices[i] -= b;
 		return result;
 	}
 
-	public readonly override bool Equals(object? obj) => obj is ConvexPolygon value && value == this;
+	public readonly override bool Equals(object? obj)
+		=> obj is ConvexPolygon value && value == this;
+
 	public readonly override int GetHashCode()
 	{
-		var hash = pointCount.GetHashCode();
-		for (int i = 0, n = pointCount * 2; i < n; i ++)
-			hash = HashCode.Combine(hash, points[i]);
+		var hash = Vertices.Count.GetHashCode();
+		for (int i = 0; i < Vertices.Count; i ++)
+			hash = HashCode.Combine(hash, Vertices[i]);
 		return hash;
 	}
+
+	public readonly StackList32<Vector2>.Enumerator GetEnumerator()
+		=> new(Vertices);
+
+	readonly IEnumerator<Vector2> IEnumerable<Vector2>.GetEnumerator()
+		=> new StackList32<Vector2>.Enumerator(Vertices);
+
+	readonly IEnumerator IEnumerable.GetEnumerator()
+		=> new StackList32<Vector2>.Enumerator(Vertices);
 }
