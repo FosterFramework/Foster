48e23c83186be68c471e7c58e69858333f7fe3b5
Vincent <94077364+Techiesplash@users.noreply.github.com>
2024-02-04 15:49:34 -0800

    Removed CLion files...?

--- Platform/cmake-build-release-visual-studio/_deps/sdl2-build/include/SDL2/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-release-2.30.0-0-g859844eae (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-release-2.30.0-0-g859844eae"
-#endif
