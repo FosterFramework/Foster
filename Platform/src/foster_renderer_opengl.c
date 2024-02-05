224f77b50456c35e58b860c98aef3719885590af
Vincent <94077364+Techiesplash@users.noreply.github.com>
2024-02-04 16:13:17 -0800

    Fixed indents further

--- Platform/src/foster_renderer_opengl.c
+++ Platform/src/foster_renderer_opengl.c
@@ -14,24 +14,24 @@
 #endif
 
 // OpenGL Value Types
-typedef ptrdiff_t		GLintptr;
-typedef ptrdiff_t		GLsizeiptr;
-typedef unsigned int	 GLenum;
-typedef unsigned char	GLboolean;
-typedef unsigned int	 GLbitfield;
-typedef void			 GLvoid;
-typedef signed char	  GLbyte;	  /* 1-byte signed */
-typedef short			GLshort;	 /* 2-byte signed */
-typedef int			  GLint;	   /* 4-byte signed */
-typedef unsigned char	GLubyte;	 /* 1-byte unsigned */
-typedef unsigned short   GLushort;	/* 2-byte unsigned */
-typedef unsigned int	 GLuint;	  /* 4-byte unsigned */
-typedef int			  GLsizei;	 /* 4-byte signed */
-typedef float			GLfloat;	 /* single precision float */
-typedef float			GLclampf;	/* single precision float in [0,1] */
-typedef double		   GLdouble;	/* double precision float */
-typedef double		   GLclampd;	/* double precision float in [0,1] */
-typedef char			 GLchar;
+typedef ptrdiff_t GLintptr;
+typedef ptrdiff_t GLsizeiptr;
+typedef unsigned int GLenum;
+typedef unsigned char GLboolean;
+typedef unsigned int GLbitfield;
+typedef void GLvoid;
+typedef signed char	GLbyte;	  /* 1-byte signed */
+typedef short GLshort;	 /* 2-byte signed */
+typedef int	GLint;	   /* 4-byte signed */
+typedef unsigned char GLubyte;	 /* 1-byte unsigned */
+typedef unsigned short GLushort;	/* 2-byte unsigned */
+typedef unsigned int GLuint;	  /* 4-byte unsigned */
+typedef int	GLsizei;	 /* 4-byte signed */
+typedef float GLfloat;	 /* single precision float */
+typedef float GLclampf;	/* single precision float in [0,1] */
+typedef double GLdouble;	/* double precision float */
+typedef double GLclampd;	/* double precision float in [0,1] */
+typedef char GLchar;
 
 // OpenGL Constants
 #define GL_DONT_CARE 0x1100
@@ -1016,9 +1016,9 @@ bool FosterInitialize_OpenGL()
 	SDL_GL_MakeCurrent(state->window, fgl.context);
 
 	// bind opengl functions
-#define GL_FUNC(name, ...) fgl.gl ## name = (gl ## name ## Fn)(SDL_GL_GetProcAddress("gl" #name));
-	GL_FUNCTIONS
-#undef GL_FUNC
+	#define GL_FUNC(name, ...) fgl.gl ## name = (gl ## name ## Fn)(SDL_GL_GetProcAddress("gl" #name));
+		GL_FUNCTIONS
+	#undef GL_FUNC
 
 	// bind debug message callback
 	if (fgl.glDebugMessageCallback != NULL && state->logLevel != FOSTER_LOGGING_NONE)
