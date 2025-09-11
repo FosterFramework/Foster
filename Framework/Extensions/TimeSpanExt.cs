f75db7ba7e75c59f2e0ef4e27d9dbacd820675a5
NoelFB <thenberry@gmail.com>
2025-09-11 15:23:47 -0700

    adding TimeSpan.Sin and TimeSpan.Cos extension methods

--- Framework/Extensions/TimeSpanExt.cs
+++ Framework/Extensions/TimeSpanExt.cs
@@ -13,4 +13,18 @@ public static class TimeSpanExt
 	/// </summary>
 	public static TimeSpan Modulo(this TimeSpan timespan, TimeSpan mod)
 		=> TimeSpan.FromTicks(timespan.Ticks % mod.Ticks);
+
+	/// <summary>
+	/// Returns a <see cref="MathF.Sin"/> value using TimeSpan by gracefully
+	/// wrapping the input value by <see cref="MathF.Tau"/> at the given <paramref name="rate"/>
+	/// </summary>
+	public static float Sin(this TimeSpan timespan, float rate = 1, float offset = 0)
+		=> MathF.Sin((float)(timespan + TimeSpan.FromSeconds(offset)).Modulo(MathF.Tau).TotalSeconds * rate);
+
+	/// <summary>
+	/// Returns a <see cref="MathF.Cos"/> value using TimeSpan by gracefully
+	/// wrapping the input value by <see cref="MathF.Tau"/> at the given <paramref name="rate"/>
+	/// </summary>
+	public static float Cos(this TimeSpan timespan, float rate = 1, float offset = 0)
+		=> MathF.Cos((float)(timespan + TimeSpan.FromSeconds(offset)).Modulo(MathF.Tau).TotalSeconds * rate);
 }
