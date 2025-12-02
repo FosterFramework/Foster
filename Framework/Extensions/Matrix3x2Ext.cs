1f0355fb700da3b70d3ec46ac072f830ebcde54b
Maddy Thorson <maddymakesgames@gmail.com>
2025-12-02 13:36:44 -0800

    Matrix3x2 extensions for calculating matrix scalle

--- Framework/Extensions/Matrix3x2Ext.cs
+++ Framework/Extensions/Matrix3x2Ext.cs
@@ -7,6 +7,36 @@ namespace Foster.Framework;
 /// </summary>
 public static class Matrix3x2Ext
 {
-	public static float ScalingFactor(this Matrix3x2 matrix)
-		=> MathF.Sqrt(matrix.M11 * matrix.M11 + matrix.M12 * matrix.M12);
+	extension(Matrix3x2 mat)
+	{
+		/// <summary>
+		/// Get the x-scale of the <see cref="Matrix3x2"/>. This will be incorrect if the matrix is rotated or skewed, but it's much cheaper than <see cref="XScale"/>
+		/// </summary>
+		public float XScaleFast => mat.M11;
+
+		/// <summary>
+		/// Get the y-scale of the <see cref="Matrix3x2"/>. This will be incorrect if the matrix is rotated or skewed, but it's much cheaper than <see cref="YScale"/>
+		/// </summary>
+		public float YScaleFast => mat.M22;
+
+		/// <summary>
+		/// Get the scale of the <see cref="Matrix3x2"/>. This will be incorrect if the matrix is rotated or skewed, but it's much cheaper than <see cref="Scale"/>
+		/// </summary>
+		public Vector2 ScaleFast => new(mat.M11, mat.M22);
+
+		/// <summary>
+		/// Get the x-scale of the <see cref="Matrix3x2"/> (this is expensive as it requires a square root - you can use <see cref="XScaleFast"/> if you know the matrix is not rotated or skewed)
+		/// </summary>
+		public float XScale => MathF.Sqrt(mat.M11 * mat.M11 + mat.M21 * mat.M21);
+
+		/// <summary>
+		/// Get the y-scale of the <see cref="Matrix3x2"/> (this is expensive as it requires a square root - you can use <see cref="YScaleFast"/> if you know the matrix is not rotated or skewed)
+		/// </summary>
+		public float YScale => MathF.Sqrt(mat.M12 * mat.M12 + mat.M22 * mat.M22);
+
+		/// <summary>
+		/// Get the scale of the <see cref="Matrix3x2"/> (this is expensive as it requires two square roots - you can use <see cref="ScaleFast"/> if you know the matrix is not rotated or skewed)
+		/// </summary>
+		public Vector2 Scale => new(mat.XScale, mat.YScale);
+	}
 }
